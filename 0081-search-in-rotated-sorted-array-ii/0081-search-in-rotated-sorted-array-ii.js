6850371c322f69f0ebc8b84080d89a6ddd9d3b45
Muhammad Muaj <muaaj24@gmail.com>
2026-05-18 19:52:10 +0600

    Time: 0 ms (100%), Space: 54.6 MB (29.74%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -6,36 +6,33 @@
 var search = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
-    
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        
-        if (nums[mid] === target) {
+    while(left <= right){
+        let mid = Math.floor((left+ right)/2)
+
+        if(nums[mid] === target){
             return true;
         }
-        
-        // NEW: Handle the duplicate ambiguity case
-        if (nums[left] === nums[mid] && nums[mid] === nums[right]) {
-            left++;
-            right--;
-            continue; // Skip the rest of the loop and recalculate mid
+        if(nums[left] === nums[mid] && nums[right] === nums[mid]){
+            left ++;
+            right --;
+            continue;
         }
-        
-        // Everything below remains exactly the same as Problem I
-        if (nums[left] <= nums[mid]) {
-            if (target >= nums[left] && target < nums[mid]) {
-                right = mid - 1;
-            } else {
+        if(nums[left] <= nums[mid]){
+            if(target >= nums[left] && target < nums[mid]){
+                right = mid - 1; 
+            }
+            else{
                 left = mid + 1;
             }
-        } else {
-            if (target > nums[mid] && target <= nums[right]) {
-                left = mid + 1;
-            } else {
+        }
+        else{
+            if(target <= nums[right] && target > nums[mid]){
+                left = mid + 1; 
+            }
+            else{
                 right = mid - 1;
             }
         }
     }
-    
-    return false;
+    return false
 };
